74ae4b78a748701268ae6cb81fbb89ce9d9c3826
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2024-02-26 02:12:11 +0000

    Sun, Feb 25, 2024, 6:12 PM -08:00

--- js/sound.js
+++ js/sound.js
@@ -1,5 +1,7 @@
 sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
-    
+    btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText=sound
 })
