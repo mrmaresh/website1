0d8cb0aaf7c6639323574a0a791a77cca04c65ed
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2024-03-04 02:51:52 +0000

    Sun, Mar 3, 2024, 6:51 PM -08:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,22 +1,28 @@
-const toggle = document.getElementById('toggle');
-const close = document.getElementById('close');
-const open = document.getElementById('open');
-const modal = document.getElementById('modal');
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+model = document.getElementById('model')
+close = document.getElementById('close')
 
-
-
-// Toggle nav
+// Toggle Nav
 toggle.addEventListener('click', () => {
-  document.body.classList.toggle('show-nav');
-});
+  document.body.classList.toggle('show-nav')
+})
 
-// Show modal
-open.addEventListener('click', () => modal.classList.add('show-modal'));
+// Show Modal
+open.addEventListener('click', () => {
+  modal.classList.add('show-model')
+})
 
-// Hide modal
-close.addEventListener('click', () => modal.classList.remove('show-modal'));
+// Hide Modal
+close.addEventListener('click', () => {
+  modal.classList.remove('show-model')
+})
 
-// Hide modal on outside click
+// Hide Model on outside click
+// window.addEventListener('click', e =>
+//   e.target == modal ? modal.classList.remove('show-modal') : false
+// )
+
 window.addEventListener('click', e =>
   e.target == modal ? modal.classList.remove('show-modal') : false
-);
+)
