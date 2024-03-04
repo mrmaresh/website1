2665d868e2d09339bda0cf7f66320a821d997f43
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2024-03-04 02:09:56 +0000

    Sun, Mar 3, 2024, 6:09 PM -08:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,6 +1,12 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+model = document.getElementById('model')
 
 
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+  modal.classList.add('show-model')
 })
