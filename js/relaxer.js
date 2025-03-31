82339736c1b2ac5defee6daa7bab82824ccbdcfd
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2025-03-31 20:09:22 +0000

    Mon, Mar 31, 2025, 1:09 PM -07:00

---
+++ js/relaxer.js
@@ -0,0 +1,20 @@
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+setInterval(breathAnimation, 7500)
+
+function breathAnimation() {
+    text.innerHTML = 'Breathe In'
+    container.classList.remove('shrink')
+    container.classList.add('grow')
+
+    setTimeout( () => {
+        text.innerHTML = 'Hold'
+    }, 3000)
+
+    setTimeout( () => {
+        text.innerHTML = 'Breathe Out!'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 4500)
+}
